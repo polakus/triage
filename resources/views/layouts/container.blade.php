18226594229f67b64485d55576eb3bf909253a98
Alejandro Gonzales <ale368_dvs@hotmail.com>
2020-08-07 12:36:09 -0300

    eliminamos todo

--- resources/views/layouts/container.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-            </div>
-        </div>
-    </div>
-</div>
