18dc93d931849a5a8415418aca40a73e60a89785
Alejandro Gonzales <ale368_dvs@hotmail.com>
2020-09-17 23:41:39 -0300

    dashboard arreglando

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,7 +3,7 @@
 @section("cuerpo")
 
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-  <h1 class="h2">Dashboard</h1>
+  <h1 class="h2">Dashboardafasdf</h1>
 	<div class="btn-toolbar mb-2 mb-md-0">
 	  <div class="btn-group mr-2">
       <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
@@ -147,38 +147,6 @@
       </div>
     </div>
   </div>
-
-  <div class="col-x1-6 col-md-6 mb-4">
-    <div class="card border-left-success shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-        <table class="tabs" data-min="0" data-max="2">
-          <tr>
-              <th class="tabcks">&nbsp;</th>
-              <th class="tabck" id="tabck-0" onclick="activarTab(this)">Primera</th>
-              <th class="tabcks">&nbsp;</th>
-              <th class="tabck" id="tabck-1" onclick="activarTab(this)">Segunda</th>
-              <th class="tabcks">&nbsp;</th>
-              <th class="tabck" id="tabck-2" onclick="activarTab(this)">Tercera</th>
-          </tr>
-          <tr class="filadiv">
-              <td colspan="6" id="tab-0">
-                  <div class="tabdiv" id="tabdiv-0">
-                      <p>id=tabdiv-0</p>...
-                  </div>
-                  <div class="tabdiv" id="tabdiv-1">
-                      <p>id=tabdiv-1</p>...
-                  </div>
-                  <div class="tabdiv" id="tabdiv-2">
-                      <p>id=tabdiv-2</p>...
-                  </div>
-              </td>
-          </tr>
-      </table>
-        </div>
-      </div>
-    </div>
-  </div>
 </div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 <script>
@@ -230,58 +198,6 @@
   });
   chartMes.render();
   chartAnio.render();
-  }
-
-  function activarTab(unTab) {
-      try {
-          //Los elementos div de todas las pestañas están todos juntos en una
-          //única celda de la segunda fila de la tabla de estructura de pestañas.
-          //Hemos de buscar la seleccionada, ponerle display block y al resto
-          //ponerle display none.
-          var id = unTab.id;
-          if (id){
-              var tr = unTab.parentNode || unTab.parentElement;
-              var tbody = tr.parentNode || tr.parentElement;
-              var table = tbody.parentNode || tbody.parentElement;
-              //Pestañas en varias filas
-              if (table.getAttribute("data-filas")!=null){
-                  var filas = tbody.getElementsByTagName("tr");
-                  var filaDiv = filas[filas.length-1];
-                  tbody.insertBefore(tr, filaDiv);
-              }
-              //Para compatibilizar con la versión anterior, si la tabla no tiene los
-              //atributos data-min y data-max le ponemos los valores que tenían antes del
-              //cambio de versión.
-              var desde = table.getAttribute("data-min");
-              if (desde==null) desde = 0;
-              var hasta = table.getAttribute("data-max");
-              if (hasta==null) hasta = MAXTABS;
-              var idTab = id.split("tabck-");
-              var numTab = parseInt(idTab[1]);
-              //Las "tabdiv" son los bloques interiores mientras que los "tabck"
-              //son las pestañas.
-              var esteTabDiv = document.getElementById("tabdiv-" + numTab);
-              for (var i=desde; i<=hasta; i++) {
-                  var tabdiv = document.getElementById("tabdiv-" + i);
-                  if (tabdiv) {
-                      var tabck = document.getElementById("tabck-" + i);
-                      if (tabdiv.id == esteTabDiv.id) {
-                          tabdiv.style.display = "block";
-                          tabck.style.color = "slategrey";
-                          tabck.style.backgroundColor = "rgb(235, 235, 225)";
-                          tabck.style.borderBottomColor = "rgb(235, 235, 225)";
-                      } else {
-                          tabdiv.style.display = "none";
-                          tabck.style.color = "white";
-                          tabck.style.backgroundColor = "gray";
-                          tabck.style.borderBottomColor = "gray";
-                      }
-                  }
-              }
-          }
-      } catch (e) {
-          alert("Error al activar una pestaña. " + e.message);
-      }
   }
 </script>
   @endsection
