b33e773a5da70fb60e4b9d78a46cedb54166b0b6
polakus <polakus368@gmail.com>
2022-01-03 21:40:16 -0300

    agregando nuevos datos de pacientes

---
+++ public/js/pacientes2.js
@@ -0,0 +1,39 @@
+export const pacientes = {
+    "DatosPacientes": [{
+        "ApellidoyNombre": "SOLA HECTOR ABEL",
+        "Edad": 73,
+        "FechaIngreso": "2021-11-12",
+        "FechaSalida": "0000-00-00",
+        "HoraIngreso": "0001-01-01T08:39:00",
+        "HoraSalida": "0000-00-00T00:00:00",
+        "MotivoIngreso": "MOTIVO",
+        "NroDocumento": "8205714",
+        "ObraSocial": "",
+        "Procedencia": ""
+    }, {
+        "ApellidoyNombre": "MALDONADO AMADEO VIDAL",
+        "Edad": 69,
+        "FechaIngreso": "2021-06-15",
+        "FechaSalida": "2021-06-15",
+        "HoraIngreso": "0001-01-01T07:00:00",
+        "HoraSalida": "0001-01-01T10:59:00",
+        "MotivoIngreso": "MOTIVO",
+        "NroDocumento": "4",
+        "ObraSocial": "000000-IPS",
+        "Procedencia": ""
+    }, {
+        "ApellidoyNombre": "BARRIO MARIA ESTER",
+        "Edad": 66,
+        "FechaIngreso": "2021-02-12",
+        "FechaSalida": "0000-00-00",
+        "HoraIngreso": "0001-01-01T11:07:00",
+        "HoraSalida": "0000-00-00T00:00:00",
+        "MotivoIngreso": "MOTIVO",
+        "NroDocumento": "11725350",
+        "ObraSocial": "",
+        "Procedencia": ""
+    },],
+    "WSMensaje": ""
+}
+
+const nombre = " alejandro";
